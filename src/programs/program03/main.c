2149a59f3608cc1a57f9392e5951a0109924397b
Mario cerryaids <trumme240053@sr.htlweiz.at>
2026-04-23 09:22:13 +0000

    Aufgabe 3/4

--- src/programs/program03/main.c
+++ src/programs/program03/main.c
@@ -6,10 +6,13 @@
 #include <stdio.h>
 
 // TODO: your code goes here
-
+double Celsius2Fahrenheit(double temp_celsius, double fahrenheit);{
+ double fahrenheit = (temp_celsius * 1,8) + 32;
+ return fahrenheit;
+}
 int main(void) {
   double temp_celsius = 0;
-
+  printf("Geben sie eine Temperatur ein: ");
   scanf("%lf", &temp_celsius);
 
   printf("%f", Celsius2Fahrenheit(temp_celsius));
--- src/programs/program04/main.c
+++ src/programs/program04/main.c
@@ -4,15 +4,20 @@
 
 #include <inttypes.h>
 #include <stdio.h>
-
+#include <math.h>
 // TODO: your code goes here
-
+double compute_hypotenuse(leg_a, leg_b, Hypotenuse){
+  double Hypotenuse = 0; 
+  Hypotenuse = sqrt((leg_a *leg_a) + (leg_b *leg_b));
+  return Hypotenuse;
+}
 int main(void) {
   double leg_a = 0, leg_b = 0;
-
+  printf("Kathete a:");
   scanf("%lf", &leg_a);
-
-  printf("%f", compute_hypotenuse(leg_a, leg_b));
+  printf("Kathete b:");
+  scanf("%lf", &leg_b);
+  printf("%lf", compute_hypotenuse(leg_a, leg_b));
 
   return 0;
 }
