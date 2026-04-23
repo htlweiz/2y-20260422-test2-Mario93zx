f9554969cbf43fd50f2cfad41d03a98ed0b14f76
Mario cerryaids <trumme240053@sr.htlweiz.at>
2026-04-23 08:43:47 +0000

    Aufgabe 1

--- src/programs/program01/main.c
+++ src/programs/program01/main.c
@@ -7,5 +7,14 @@
 
 int main(int argc, char **argv) {
    // write your solution here
-
+   int number = 0;
+   printf("Geben sie eine Zahl ein: \n");
+   scanf("%d" ,&number);
+   if (number > 0){
+   for (int i = 0; i = number; i = i +1)
+   printf("%d ", i);
+   }else{
+   for (int i = 0; i = number; i -= -1)
+   printf ("%d" , i);
+   }
 }
